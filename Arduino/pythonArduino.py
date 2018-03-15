7b77cf67a114f0badd41d6a8d0028c074f5fbec6
AkashaAhirwar <akashahirwar33@gmail.com>
2018-03-16 01:30:38 +0530

    Modified pythonArduino.py

--- Arduino/pythonArduino.py
+++ Arduino/pythonArduino.py
@@ -2,22 +2,20 @@ import serial
 import time
 
 i=serial.Serial('/dev/ttyACM0', 9600)
-ON = int(1)
-OFF = int(0)
 
-OnTime = int(input("Enter the \"OnTime\" \n"))
-OffTime = int(input("Enter the \"OffTime\"\n"))
-TotalTime = int(input("Enter the time duration of the experiment \n"))
-
-start = time.time()
-current = start
-while (current - start) < TotalTime):
-        i.write(ON)
+Off=(0)
+OnTime=int(input("OnTime\n"))
+OffTime=int(input("OffTime\n"))
+TotalTime=int(input("TotalTime\n"))
+intensity=int(input("Voltage\n"))
+s=time.time()
+e=time.time()
+while ((e-s)<TotalTime):
+        print(intensity,e-s)
+        i.write(str(intensity).encode())
         time.sleep(OnTime)
-        current = time.time()
-        if ( current - start > TotalTime):
-            break
-        i.write(OFF)
+        i.write(str(0).encode())
         time.sleep(OffTime)
+        e=time.time()
 
-print("done")
+i.write(str(0).encode())
