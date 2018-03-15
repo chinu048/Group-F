7899cd91554fa41cd60755b106a1e859bf6dd03b
ameykpatel <ameykpatel@gmail.com>
2018-03-15 23:37:30 +0530

    Added signup.py

---
+++ GUI/signup.py
@@ -0,0 +1,92 @@
+
+from PyQt4 import QtCore, QtGui
+import sqlite3
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_signUp(object):
+    def insertData(self):
+        username = self.uname_lineEdit.text()
+        email = self.email_lineEdit.text()
+        password = self.password_lineEdit.text()
+
+        connection  = sqlite3.connect("login.db")
+        connection.execute("INSERT INTO USERS VALUES(?,?,?)",(username,email,password))
+        connection.commit()
+        connection.close()
+
+
+
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(570, 375)
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(160, 130, 81, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(160, 230, 81, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.label_3 = QtGui.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(160, 180, 81, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.uname_lineEdit = QtGui.QLineEdit(Dialog)
+        self.uname_lineEdit.setGeometry(QtCore.QRect(250, 130, 141, 20))
+        self.uname_lineEdit.setObjectName(_fromUtf8("uname_lineEdit"))
+        self.email_lineEdit = QtGui.QLineEdit(Dialog)
+        self.email_lineEdit.setGeometry(QtCore.QRect(250, 180, 141, 20))
+        self.email_lineEdit.setObjectName(_fromUtf8("email_lineEdit"))
+        self.password_lineEdit = QtGui.QLineEdit(Dialog)
+        self.password_lineEdit.setGeometry(QtCore.QRect(250, 230, 141, 20))
+        self.password_lineEdit.setObjectName(_fromUtf8("password_lineEdit"))
+        self.signup_btn = QtGui.QPushButton(Dialog)
+        self.signup_btn.setGeometry(QtCore.QRect(270, 290, 75, 23))
+        self.signup_btn.setObjectName(_fromUtf8("signup_btn"))
+
+        self.label_4 = QtGui.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(150, 10, 321, 81))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.label_4.setFont(font)
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Sign-Up Form", None))
+        self.label.setText(_translate("Dialog", "USERNAME", None))
+        self.label_2.setText(_translate("Dialog", "PASSWORD", None))
+        self.label_3.setText(_translate("Dialog", "EMAIL ID", None))
+        self.signup_btn.setText(_translate("Dialog", "Sign Up", None))
+        self.label_4.setText(_translate("Dialog", "Create Account", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Dialog = QtGui.QDialog()
+    ui = Ui_signUp()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
