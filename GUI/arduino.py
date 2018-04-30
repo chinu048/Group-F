9e96d1635e751929c152ab3118fef441dd25f1be
AkashAhirwar <35365498+AkashAhirwar@users.noreply.github.com>
2018-05-01 04:09:49 +0530

    Update arduino.py

--- GUI/arduino.py
+++ GUI/arduino.py
@@ -3,9 +3,9 @@ import time
 #i=serial.Serial('/dev/ttyACM0', 9600)
 i=serial.Serial('COM3', 9600)
 def ard(ontime,offtime,totaltime):
-        time.sleep(1)
+        time.sleep(2)
         i.write(str(ontime).encode())
-        time.sleep(1)
+        time.sleep(2)
         i.write(str(offtime).encode())
-        time.sleep(1)
+        time.sleep(2)
         i.write(str(totaltime).encode())
