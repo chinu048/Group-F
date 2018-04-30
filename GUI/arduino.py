05710dc57275c51d7ba19348aeb3b01b30b29f19
AkashAhirwar <35365498+AkashAhirwar@users.noreply.github.com>
2018-05-01 03:15:34 +0530

    Minor changes

--- GUI/arduino.py
+++ GUI/arduino.py
@@ -1,18 +1,11 @@
 import serial
 import time
-
 #i=serial.Serial('/dev/ttyACM0', 9600)
 i=serial.Serial('COM3', 9600)
-
 def ard(ontime,offtime,totaltime):
-	s=time.time()
-	e=time.time()
-	while ((e-s)<totaltime):
-        	i.write(str(1).encode())
-        	time.sleep(ontime)
-        	i.write(str(0).encode())
-        	time.sleep(offtime)
-        	e=time.time()
-
-i.write(str(0).encode())
-
+        time.sleep(1)
+        i.write(str(ontime).encode())
+        time.sleep(1)
+        i.write(str(offtime).encode())
+        time.sleep(1)
+        i.write(str(totaltime).encode())
